23adfa1454bac887cdc7b3ceb9c732dacf7c6b0f
Marsaleix Romain <msx.coach@gmail.com>
2022-03-10 21:44:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
+import Choice from './Public/screens/Choice';
+import Welcome from './Public/screens/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/Choice" element={<Choice />} />
+    </Routes>
     </div>
   );
 }
---
+++ src/Public/screens/Choice.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style/Choice.css'
+
+const Choice = () => {
+  return (
+    <div className='Choice-page'>
+        <div className="navbar">
+            <div className="logo2"></div>
+            <div className="name"></div>
+            <div className='bloc-log'>
+                <div className="admin">Connection Admin</div>
+                <div className="creation-login"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Choice
