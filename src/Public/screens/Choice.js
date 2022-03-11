f0298bbe7aec7f40a88cdf4e8309e4d3080a8a3d
Marsaleix Romain <msx.coach@gmail.com>
2022-03-11 09:43:02 +0100

    creation site, mise en place router, choice : creation navbar + 3 items

--- src/Public/screens/Choice.js
+++ src/Public/screens/Choice.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './style/Choice.css'
+import icon from '../../Zassets/profil(1).png'
 
 const Choice = () => {
   return (
     <div className='Choice-page'>
+      
         <div className="navbar">
             <div className="logo2"></div>
-            <div className="name"></div>
+            <div className="name"><h1>JOYSTICK</h1>
+    <h3>La passion du jeu vidéo</h3></div>
             <div className='bloc-log'>
-                <div className="admin">Connection Admin</div>
-                <div className="creation-login"></div>
+            <img src={icon} alt={icon} className="logoID"/>
+                <div className="admin">Se connecter</div>
+                <hr />
+                <div className="creation-login">S'enregistrer</div>
             </div>
         </div>
     </div>
